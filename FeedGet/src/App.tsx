50505de04c3c51f0ee27167b6df58aef4e5bffcc
Danielsear <danielandradeseara@hotmail.com>
2022-06-05 21:48:52 -0300

    texto

--- FeedGet/src/App.tsx
+++ FeedGet/src/App.tsx
@@ -1,7 +1,14 @@
 import { Widget } from './components/Widget'
 
 function App() {
-  return <Widget />
+  return (
+    <>
+      <h1 className="text-write">
+        OLHA O APP JULIANA, nao vou te pagar 50 reais
+      </h1>
+      <Widget />
+    </>
+  )
 }
 
 export default App
