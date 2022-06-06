c089688e23d4ca2a9af5b6d4b9561acd664cc2e5
Danielsear <danielandradeseara@hotmail.com>
2022-06-05 21:55:39 -0300

    back

--- FeedGet/src/App.tsx
+++ FeedGet/src/App.tsx
@@ -1,14 +1,7 @@
 import { Widget } from './components/Widget'
 
 function App() {
-  return (
-    <>
-      <h1 className="text-write">
-        OLHA O APP JULIANA, nao vou te pagar 50 reais
-      </h1>
-      <Widget />
-    </>
-  )
+  return <Widget />
 }
 
 export default App
